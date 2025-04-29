a243771acf808ed38a485964d097318546e4aa68
swlws <979623074@qq.com>
2025-04-29 21:02:50 +0800

    双栈模拟队列

---
+++ src/algo/queue/queue-using-stacks/index.mjs
@@ -0,0 +1,61 @@
+import { Stack } from '../../../_structure/stack/index.mjs'
+var MyQueue = function () {
+  this.mainStack = new Stack()
+  this.subStack = new Stack()
+}
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+  if (this.mainStack.isEmpty()) {
+    this.mainStack.push(x)
+  } else {
+    while (!this.mainStack.isEmpty()) {
+      this.subStack.push(this.mainStack.pop())
+    }
+
+    this.mainStack.push(x)
+
+    while (!this.subStack.isEmpty()) {
+      this.mainStack.push(this.subStack.pop())
+    }
+  }
+}
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+  if (this.mainStack.isEmpty()) {
+    return null
+  }
+  return this.mainStack.pop()
+}
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+  if (this.mainStack.isEmpty()) {
+    return null
+  }
+  return this.mainStack.peek()
+}
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+  return this.mainStack.isEmpty()
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
