1cc51e12717a6926dfe515f8e9600ebc6b0af4d7
swlws <979623074@qq.com>
2025-08-12 09:26:26 +0800

    sort

---
+++ src/algo/sort/bubbleSort.mjs
@@ -0,0 +1,26 @@
+/**
+ * 冒泡排序
+ * 时间复杂度：O(n^2)
+ * 空间复杂度：O(1)
+ * 稳定
+ * 比较相邻的元素。如果第一个比第二个大，就交换他们两个。
+ * 对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对。在这一点，最后的元素应该会是最大的数。
+ * 针对所有的元素重复以上的步骤，除了最后一个。
+ * 持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较。
+ * @param {*} arr
+ * @returns
+ */
+function bubbleSort(arr) {
+  const len = arr.length
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+      }
+    }
+  }
+  return arr
+}
+
+const arr = [1, 3, 2, 4, 5, 6, 7, 8, 9]
+console.log(bubbleSort(arr))
