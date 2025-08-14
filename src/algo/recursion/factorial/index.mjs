bbc87f5f0cf79ce8e28a391f41ada5fa5813edfd
swlws <979623074@qq.com>
2025-08-14 15:26:23 +0800

    code

--- src/algo/recursion/factorial/index.mjs
+++ src/algo/recursion/factorial/index.mjs
@@ -1,8 +1,12 @@
+/**
+ * 阶乘 - 递归
+ * @param {*} n
+ * @returns
+ */
 function factorial(n) {
   if (n < 0) {
     throw new Error('Negative numbers are not allowed')
   }
-  console.log(n)
   if (n === 0 || n === 1) {
     return 1
   }
@@ -10,3 +14,18 @@ function factorial(n) {
 }
 
 console.log('阶乘', factorial(10))
+
+/**
+ * 阶乘 - 动态规划
+ * @param {*} n
+ * @returns
+ */
+function factorialDP(n) {
+  const dp = [1, 1]
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] * i
+  }
+  return dp[n]
+}
+
+console.log('阶乘DP', factorialDP(10))
